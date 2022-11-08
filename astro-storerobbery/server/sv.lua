3709b70d08bddcdd13629d6edb8d4def6fc0615d
katakume <72819680+katakume-astro@users.noreply.github.com>
2022-11-08 18:14:53 +0100

    Update sv.lua

--- astro-storerobbery/server/sv.lua
+++ astro-storerobbery/server/sv.lua
@@ -2,23 +2,18 @@
 print('Astro-Store-Robbery | katakume#5873')
 
 
-RegisterNetEvent('PowodzenieNapad')
-AddEventHandler('PowodzenieNapad', function()
-	local pieniazki = math.random(400, 1000)
-	local los = math.random(1, 10)
-	exports.ox_inventory:AddItem(source, 'money', pieniazki)
-	if los >= 4 then
-		--exports.ox_inventory:AddItem(source, 'safecracker', 1) BEDE ROZWIJAL TEN SKRYPT PO NAPADY NA SEJFY ale to potem!
-		print('true')
-	end
+RegisterNetEvent('astro-storerobbery:server:succes')
+AddEventHandler('astro-storerobbery:server:succes', function()
+	local money = math.random(400, 1000)
+	exports.ox_inventory:AddItem(source, 'money', money)
 end)
 
-RegisterNetEvent('napadzlewszytskozle')
-AddEventHandler('napadzlewszytskozle', function()
+RegisterNetEvent('astro-storerobbery:server:removeItem')
+AddEventHandler('astro-storerobbery:server:removeItem', function()
 	exports.ox_inventory:RemoveItem(source, 'lockpick', 1)
 end)
 
-ESX.RegisterServerCallback('getPoliceCount', function(src, cb) -- FROM ESX DISCORD 
+ESX.RegisterServerCallback('getPoliceCount', function(src, cb)
 	local policeTable = ESX.GetExtendedPlayers("job", "police")
 	local policeCount = 0
   
