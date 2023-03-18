15e6adc0a3282809b132f8caac1cd88baf83d308
katakume <72819680+katakume-astro@users.noreply.github.com>
2023-03-18 23:44:03 +0100

    callbacks

--- astro-storerobbery/server/sv.lua
+++ astro-storerobbery/server/sv.lua
@@ -2,11 +2,10 @@
 print('Astro-Store-Robbery | katakume#5873')
 
 
-RegisterNetEvent('astro-storerobbery:server:succes')
-AddEventHandler('astro-storerobbery:server:succes', function()
+lib.callback.register('astro-storerobbery:server:succes', function()
 	local money = math.random(400, 1000)
 	exports.ox_inventory:AddItem(source, 'money', money)
-        exports['astro_lib']:log(source, 'Triggered event : astro-storerobbery:server:succes')
+        exports['astro_lib']:log(source, 'Gracz wykonal napad na kasetke')
 end)
 
 RegisterNetEvent('astro-storerobbery:server:removeItem')
